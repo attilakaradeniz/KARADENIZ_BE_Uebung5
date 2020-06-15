2944566cc0b909db1cab65a8d66ab96e88a9bb51
attilakaradeniz <attilakaradeniz@gmail.com>
2020-06-15 13:34:59 +0200

    !format but passengers done

--- api/controller/FlightsController.php
+++ api/controller/FlightsController.php
@@ -25,12 +25,39 @@ class FlightsController
                     $flightId = filter_input(INPUT_GET, 'flightId',FILTER_SANITIZE_NUMBER_INT);
                     //$passengersSend = $this->showPassengers($flightId);
                     //$this->jsonView->output($passengersSend);
+                if(!$flightId)
+                {
+                    $this->jsonView->output(
+                        ["An error occurred:" => "You have to specify a flight id for the respective passenger list",
+                        "After GET_PASSENGERS provide an id with the parameter prefix {flightId} like this:" => "&flightId={id}"]);
+                }
+                else
+                {
+                    //echo "passengers"; // TEST
+                    //$this->jsonView->output($this->showPassengers($flightId));
+                    //print_r($this->showPassengers($flightId));
+                    $passengerList = [];
+                    //print_r($passengerList);
+                    $passengers = $this->showPassengers($flightId);
+                    //print_r($passengers);
 
-                    $this->jsonView->output($this->showPassengers($flightId));
+                    //$passengerList['flightName'] = $passengers[0]['flightname'];
+                    //print_r($passengerList);
+                    //$this->jsonView->output($passengerList);
+                    foreach ($passengers as $passenger) {
+//                        $passengerList['passengers'][] = array('lastname' => $passenger['lastname'], 'firstname' => $passenger['firstname']);
+                        //$passengerList['flightName'][] = array($passenger['flightname']);
+                        //print_r($passenger);
+                        $passengerList['passengers'][] = array('flightName' => $passenger['flightname'], 'lastname' => $passenger['lastname'], 'firstname' => $passenger['firstname']);
+                        //print_r($passengerList);
+                    }
+                    $this->jsonView->output($passengerList);
+
+                    //$this->showPassengers($passengerList);
+                }
                     break;
-
-
-
+                default : $this->jsonView->output(["An Error occurred!" => "Interface not found!", "Possible parameters for action are: " => "GET-FLIGHTS, GET_PASSENGERS&FLIGHTID=[id]"]);
+                //return false;
             }
 
         }
